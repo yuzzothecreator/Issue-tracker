e49737382f97840a1c18d80a0fc51fedca4ddb96
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-08 11:32:48 +0300

    implement clieent side validation

--- app/api/issues/new/page.tsx
+++ app/api/issues/new/page.tsx
@@ -1,21 +1,23 @@
 "use client"
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, handleSubmit, control } = useForm<IssueForm>();
+    const { register, handleSubmit, control, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('');
 
   return (
@@ -34,11 +36,13 @@ const NewIssuePage = () => {
         <TextField.Root>
             <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && <Text as='p' color='red'>{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
           render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
         />
+        {errors.description && <Text as="p" color='red'>{errors.description.message}</Text>}
         <Button>Submit New Issues</Button>
     </form>
     </div>
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import  prisma  from "@/prisma/client";
-import { z } from "zod";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, 'Description is required'),
-});
+import { createIssueSchema } from "../../validationSchema";
 
 export async function POST(request: NextRequest) {
   try {
