5d0785ea3f9144bc37f9bb08641d78039d2fb27d
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-12 04:58:58 +0300

    improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,21 +1,19 @@
 "use client"
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import { useForm, Controller, set } from 'react-hook-form';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { issueSchema } from '@/app/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Issue } from '@prisma/client';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { issueSchema } from '@/app/validationSchema';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
 import { z } from 'zod';
-import ErrorMessage from '@/app/components/ErrorMessage';
-import Spinner from '@/app/components/Spinner';
-import dynamic from 'next/dynamic';
-import { Issue } from '@prisma/client';
 
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
-import IssueForm from '../_components/IssueForm'
+
+import dynamic from 'next/dynamic';
+import IssueFormSkeleton from '../_components/IssueFormSkeleton';
+
+
+const IssueForm = dynamic(
+  () => import('../_components/IssueForm'),
+   { ssr: false,
+   loading: () => <IssueFormSkeleton />
+  });
 
 const NewIssuePage = () => {
   return (
