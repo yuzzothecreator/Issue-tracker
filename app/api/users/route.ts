c58bea29c92dfedc1f23632a9f2c49dd4a1487d2
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-13 16:45:44 +0300

    Setup react query

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,18 +1,8 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(request: NextRequest) {
-//    const users = await prisma.user.findMany({ orderBy: { name: "asc" } })
-//    return NextResponse.json(users)
-// }
-
-
-
-
-export async function GET() {
-  const users = await prisma.user.findMany({
-    orderBy: { name: "asc" }
-  });
-
-  return NextResponse.json(users);
+export async function GET(request: NextRequest) {
+   const users = await prisma.user.findMany({ orderBy: { name: "asc" } })
+   return NextResponse.json(users)
 }
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from 'next/font/google'
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from './NavBar'
 import AuthProvider from "./auth/Provider";
+import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -25,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
           <Theme appearance="light" accentColor="jade">
           <NavBar />
           <main className="p-5">
@@ -36,6 +38,7 @@ export default function RootLayout({
           {/* <ThemePanel /> */}
         </Theme>
         </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
