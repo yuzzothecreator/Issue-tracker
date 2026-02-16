263aae48003a60bc964eb4327578f4506c74a201
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-16 18:25:06 +0300

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Heading } from '@radix-ui/themes'
 import Image from 'next/image'
 import Link from 'next/link'
+import Pagination from './components/Pagination'
 
 export default function Home() {
   return (
    <main>
-    <Heading color='green' >Yuzzo The Creator</Heading>
+    {/* <Heading color='green' >Yuzzo The Creator</Heading> */}
     
+    <Pagination currentPage={2} itemCount={100} pageSize={10}/>
    </main>
   )
 }
