ea6e355262615cf6893c8b34819e4850221f44b2
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-16 18:43:27 +0300

    implement pagnation

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Pagination from './components/Pagination'
 
-export default function Home() {
+export default function Home({searchParams}: {searchParams: {page: string}}) {
   return (
    <main>
     {/* <Heading color='green' >Yuzzo The Creator</Heading> */}
     
-    <Pagination currentPage={2} itemCount={100} pageSize={10}/>
+    <Pagination currentPage={parseInt(searchParams.page) || 1} itemCount={100} pageSize={10}/>
    </main>
   )
 }
