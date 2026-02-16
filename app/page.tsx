173c20b3b496e277b0dd074f42cd2fa9dc284b83
yuzzothecreator <yuzzothecreator@gmail.com>
2026-02-16 21:32:29 +0300

    Build latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Heading } from '@radix-ui/themes'
+import LatestIssues from './LatestIssues'
 
 export default function Home({searchParams}: {searchParams: {page: string}}) {
   return (
    <main>
     <Heading color='green' >Yuzzo The Creator</Heading>
-    
+    <LatestIssues />
    </main>
   )
 }
